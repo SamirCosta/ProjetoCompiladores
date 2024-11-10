5abecd7e79dbbdea47f4fc7be86a005a2bc190fc
SamirCosta <samircfaduli@gmail.com>
2024-11-10 16:19:28 -0300

    commit

--- GeneratedCode.java
+++ GeneratedCode.java
@@ -4,10 +4,42 @@ import java.util.Scanner;
 public class GeneratedCode {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-		int x;
-		x= 0;
-		if(x==0){
-		System.out.println(x+5);
+		int contador;
+		String mensagem;
+		double taxa;
+		boolean ativo;
+		taxa= 2.1+56*10;
+		System.out.println(taxa);
+		contador= 0;
+		taxa= 0;
+		ativo= true;
+		if(contador==0){
+		if(taxa==0){
+		System.out.println("FOI");
+		}
+		taxa= 214.4351;
+		System.out.println(taxa);
+		mensagem= "Contador inicializado";
+		System.out.println(mensagem);
+		}else{
+		mensagem= "Contador já possui valor";
+		System.out.println(mensagem);
+		}
+		while(contador<10){
+		System.out.println("Contagem: "+contador);
+		contador= contador+1;
+		}
+		int i;
+		for(i= 0;i<5;i=i+2){
+		System.out.println("Iteração do loop for: "+i);
+		}
+		//skanivorovat(tseloye cont);
+		
+String mens = scanner.nextLine();		//skanivorovat(plavat tax);
+		if(ativo==true){
+		System.out.println("O sistema está ativo.");
+		}else{
+		System.out.println("O sistema está inativo.");
 		}
 		
     
